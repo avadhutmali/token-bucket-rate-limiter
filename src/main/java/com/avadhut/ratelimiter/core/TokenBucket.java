06a3b05334f3aca782cedaacc9572706aaecda12
avadhutmali <mali.avadhut7@gmail.com>
2026-07-03 19:40:38 +0530

    feat: add reentrant lock for the safety

--- src/main/java/com/avadhut/ratelimiter/core/TokenBucket.java
+++ src/main/java/com/avadhut/ratelimiter/core/TokenBucket.java
@@ -1,6 +1,9 @@
 package com.avadhut.ratelimiter.core;
 
 
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.locks.ReentrantLock;
+
 public class TokenBucket {
     private long maxTokens;
     private long currentTokens;
@@ -13,20 +16,39 @@ public class TokenBucket {
         this.refillRatePerSecond = refillRatePerSecond;
         this.lastRefilTime = System.nanoTime();
     }
+
+    private final ReentrantLock lock = new ReentrantLock();
+
     public boolean tryConsume(){
-        long currentTime = System.nanoTime();
-        long timeElapsed = currentTime - lastRefilTime;
-
-        lastRefilTime = currentTime;
-
-        long tokensToAdd = (timeElapsed/1_000_000_000L) * refillRatePerSecond;
-
-        currentTokens = Math.min(currentTokens + tokensToAdd, maxTokens);
-
-        if(currentTokens>=1){
-            currentTokens--;
-            return true;
+        try {
+            if(!lock.tryLock(50, TimeUnit.MILLISECONDS)){
+                return false;
+            }
         }
-        return false;
+        catch (InterruptedException e){
+            Thread.currentThread().interrupt();
+            return false;
+        }
+
+        try {
+            long currentTime = System.nanoTime();
+            long timeElapsed = currentTime - lastRefilTime;
+
+            lastRefilTime = currentTime;
+
+            long tokensToAdd = (timeElapsed/1_000_000_000L) * refillRatePerSecond;
+
+            currentTokens = Math.min(currentTokens + tokensToAdd, maxTokens);
+
+            if(currentTokens>=1){
+                currentTokens--;
+                return true;
+            }
+            return false;
+        } finally {
+            lock.unlock();
+        }
+
+
     }
 }
--- src/test/java/com/avadhut/ratelimiter/core/TokenBucketTest.java
+++ src/test/java/com/avadhut/ratelimiter/core/TokenBucketTest.java
@@ -2,6 +2,11 @@ package com.avadhut.ratelimiter.core;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicInteger;
+
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 
@@ -28,6 +33,25 @@ public class TokenBucketTest {
         tokenBucket.tryConsume();
         Thread.sleep(2000);
         assertTrue(tokenBucket.tryConsume(),"Should be true as 2 sec elasped");
+    }
+
+    @Test
+    public void shouldHandleConcurrency() throws InterruptedException {
+        TokenBucket tokenBucket = new TokenBucket(10,0);
+        int threadCounts = 100;
+        ExecutorService service = Executors.newFixedThreadPool(threadCounts);
+        AtomicInteger allowedCount = new AtomicInteger();
+
+        for(int i=0;i<threadCounts;i++){
+            service.submit(()->{
+                if(tokenBucket.tryConsume())allowedCount.incrementAndGet();
+            });
+        }
+        service.shutdown();
+        service.awaitTermination(5, TimeUnit.SECONDS);
+
+        assertTrue( allowedCount.get()<=10, "More spend Detected "+ allowedCount.get());
+
     }
 
 }
